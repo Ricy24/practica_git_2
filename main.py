e2246f734ba782dc7bd24afe4b6f2e1c7402805a
Brayan valderrama <rubianostivenbrayan13@gmail.com>
2026-02-05 15:33:53 -0500

    calculoIMC

--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 
 def calcular_IMC(peso, altura):
     pass
+peso = float(input("Ingresa tu peso en kg: "))
+altura = float(input("Ingresa tu altura en metros (ejemplo: 1.75): "))
+
+imc = peso / (altura ** 2)
+
+print(f"Tu IMC es: {round(imc, 2)}")
 
 def calcular_porcentaje_grasa(peso, altura, edad, valor_genero):
     pass
