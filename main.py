52d8ff1f453fe0dffa03494ca64d84f3e029b4fa
ricy24 <anriduco2019@gmail.com>
2026-02-05 14:48:18 -0500

    Plantilla Calculadora

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Calculadora de Salud - Equipo
+"""
+
+def calcular_imc(peso, altura):
+    """Calcula el Índice de Masa Corporal"""
+    pass
+
+
+def calcular_grasa_corporal(imc, edad):
+    """Calcula el porcentaje de grasa corporal"""
+    pass
+
+
+
+def calcular_tmb(peso, altura, edad, sexo):
+    """Calcula la tasa metabólica basal"""
+    pass
+
+
+def calcular_calorias_adelgazar(tmb, actividad):
+    """Calcula calorías diarias para adelgazar"""
+    pass
+
+
+if __name__ == "__main__":
+    print("Calculadora de Salud")
